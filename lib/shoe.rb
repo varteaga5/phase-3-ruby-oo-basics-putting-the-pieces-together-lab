910f35d13f650c82e1794d75e573365c474dab92
vincent <vincent.arteaga@gmail.com>
2021-08-05 11:16:17 -0700

    commit tree e8005ca7ce202ed9655fb24763e94d520e5038c3

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+    # properties 
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    # init with a brand
+    def initialize(brand)
+        @brand = brand
+    end
+
+    # methods
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
