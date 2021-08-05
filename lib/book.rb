45c7d827835aa4e5ff3c8a8431334707e22d0eac
vincent <vincent.arteaga@gmail.com>
2021-08-05 11:16:58 -0700

    lab complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    # properties
+    attr_accessor :title, :author, :page_count, :genre
+
+    # init with a title
+    def initialize(title)
+        @title = title   
+    end
 
 
+    # methods
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
